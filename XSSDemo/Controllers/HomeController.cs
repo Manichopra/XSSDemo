1790b4841429fc66276fe0c18a40057d9061d483
Manish Chopra <manish.chopra@kainos.com>
2021-11-12 14:34:25 +0000

    first commit

---
+++ XSSDemo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Ganss.XSS;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using XSSDemo.Models;
+
+namespace XSSDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Search());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Search model)
+        {
+            var sanitizer = new HtmlSanitizer();
+
+            var searchModel = new Search()
+            {
+                SearchText = sanitizer.Sanitize(model.SearchText)
+            };
+
+            return View(searchModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
